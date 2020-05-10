29effaff86821b2c3c292038eb1c39db5c938596
Steve Lemuel <wlemuel@hotmail.com>
2020-05-10 23:28:37 +0800

    rebuilding site at [Sun May 10 23:28:37 CST 2020]

---
+++ js/baidu-analyse.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0356ab0fd3c3575c003b18dd9091266c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
